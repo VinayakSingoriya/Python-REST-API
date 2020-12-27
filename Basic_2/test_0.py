4b2fbf32653a05994e0849d78c2df326d3fa1dfc
Vinayak Singoriya <vinayaksingoriya@gmail.com>
2020-12-28 00:28:02 +0530

    delete api added

--- Basic_2/test_0.py
+++ Basic_2/test_0.py
@@ -20,5 +20,22 @@ BASE = "http://127.0.0.1:5000/"
 
 
 # for test_4.py
-response = requests.get(BASE + "video/1")
+# response = requests.get(BASE + "video/1")
+# print(response.json())
+
+
+# for test_5.py
+data = [{"likes":10, "views":150, "name":"cpp"},
+       {"likes":20, "views":200, "name":"java"},
+       {"likes":30, "views":300, "name":"python"}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE+"video/2")
 print(response.json())
--- Basic_2/test_5.py
+++ Basic_2/test_5.py
@@ -1,4 +1,4 @@
-# If the get data(video_id) is not in our data, then we can abort the request with a message. 
+# Delete a video and dont create a video that already exists.
 
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
@@ -17,20 +17,35 @@ def abort_if_videoID_doesnot_exist(video_id):
     if video_id not in videos:
         abort(404, message = "Couldn't find video...")
 
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that id..")
+        
 class Video(Resource):
     def get(self, video_id):        # Used to get something
         abort_if_videoID_doesnot_exist(video_id)
         return videos[video_id]
 
-    def put(self, video_id):         # Used to create something
+    def put(self, video_id):
+        abort_if_video_exists(video_id)         # Used to create something
         args = video_put_args.parse_args()
         # print(args)
         videos[video_id] = args
         return videos[video_id], 201
 
+    def delete(self, video_id):
+        abort_if_videoID_doesnot_exist(video_id)
+        del videos[video_id]
+        return " ",204  
+
                 
         
 api.add_resource(Video, "/video/<int:video_id>")
+
+
+                
+        
+
 
 if __name__ == "__main__":
     app.run(debug = True)
